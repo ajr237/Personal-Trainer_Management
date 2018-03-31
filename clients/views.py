944ba9fc8dd331b1a22c3a496ccd2c2d1e7870b6
Andrew <aj.rushton@icloud.com>
2018-03-31 16:26:14 +0100

    Updated template structure.

--- clients/views.py
+++ clients/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse('<h1>PT Manager Homepage</h1>')
+
+def index2(request):
+    return render(request, 'index.html', {})
