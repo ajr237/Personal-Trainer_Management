166f82a49bd762fbdc88579dace75bc601fad695
Andrew <aj.rushton@icloud.com>
2018-04-04 19:41:10 +0100

    updated signup page

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,11 +1,23 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
-class UserCreateform(UserCreationForm):
+class UserCreationForm(UserCreationForm):
     class Meta():
         fields = ('username', 'email', 'password1', 'password2')
         model = get_user_model()
 
     def __init__(self, *args, **kwargs):
-        super(MyForm, self).__init__(*args, **kwargs)
+        super(UserCreationForm, self).__init__(*args, **kwargs)
         self.fields['email'].widget.attrs['placeholder'] = self.fields['email'].label or 'email@address.nl'
+
+# class UserCreationForm(UserCreationForm):
+#
+#     def __init__(self, *args, **kwargs):
+#         super(UserCreationForm, self).__init__(*args, **kwargs)
+#         self.fields['username'].widget = TextInput(attrs = {'class': 'form-control',})
+#         self.fields['password1'].widget = PasswordInput(attrs = {'class': 'form-control',})
+#         self.fields['password2'].widget = PasswordInput(attrs = {'class': 'form-control',})
+#
+#     class Meta:
+#         model = User
+#         fields = UserCreationForm.Meta.fields
