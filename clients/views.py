2aa8b05573c2a7377c00031c018d5057abfab841
Andrew <aj.rushton@icloud.com>
2018-04-08 22:38:11 +0100

    added static files

--- clients/views.py
+++ clients/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, HttpResponse
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def dashboard(request):
     return render(request, 'clients/dashboard.html', {})
